ebaced61f470f13d49dc7cd170b572bdb38a4c03
KiwiLss <m15205622327@163.com>
2017-12-19 17:13:24 +0800

    customview

--- customviewone/src/main/java/com/magicsoft/customviewone/widget/canvas/PictureCancas.java
+++ customviewone/src/main/java/com/magicsoft/customviewone/widget/canvas/PictureCancas.java
@@ -1,13 +1,17 @@
 package com.magicsoft.customviewone.widget.canvas;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Picture;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.magicsoft.customviewone.R;
 
 /**
  * <pre>
@@ -20,7 +24,10 @@ import android.view.View;
  */
 
 
-public class PictureCancas extends View {//画picture
+public class PictureCancas extends View {
+    private Bitmap bitmap;
+    private Paint mPaint;
+
     public PictureCancas(Context context) {
         this(context,null);
     }
@@ -33,28 +40,36 @@ public class PictureCancas extends View {//画picture
         super(context, attrs, defStyleAttr);
         init(context);
         //调用录制方法
-        recording();
+        //recording();
     }
 
     // 1.创建Picture
     private Picture mPicture = new Picture();
     private void init(Context context) {
 
-
-
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.skin_main_bg);
+        mPaint = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //开始绘制录制内容
+    /*    //开始绘制录制内容
+        //方法一,PS：这种方法在比较低版本的系统上绘制后可能会影响Canvas状态，所以这种方法一般不会使用。
+        //mPicture.draw(canvas);
+        //方法二
+        //canvas.drawPicture(mPicture,new RectF(0,0,mPicture.getWidth(),200));*/
+
         //方法一
-        mPicture.draw(canvas);
+        canvas.drawBitmap(bitmap,100,100,mPaint);
+
+        //方法二
+        canvas.drawBitmap(bitmap,new Matrix(),mPaint);
     }
 
-    /**
+  /*  *//**
      * 录制方法
-     */
+     *//*
     private void recording(){
         // 开始录制 (接收返回值Canvas)
         Canvas canvas = mPicture.beginRecording(500, 500);
@@ -72,6 +87,6 @@ public class PictureCancas extends View {//画picture
         mPicture.endRecording();
 
     }
-
+*/
 
 }
